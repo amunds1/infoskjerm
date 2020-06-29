4acfecb9a2abd0032e1a3dfe00d33bdf9bcceff1
aa1202 <andreas.amundsen123@gmail.com>
2020-06-29 13:42:46 +0200

    Updates

---
+++ precache-manifest.978da78177350140c8142162cf1b4d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909c71db5ad97af67cfd7e252720bde7",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "ca7f13fd1f7b337b9928",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d6a077766428abf3bd7d",
+    "url": "/infoskjerm/static/js/2.7eedfa2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.7eedfa2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7f13fd1f7b337b9928",
+    "url": "/infoskjerm/static/js/main.a3551b85.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
