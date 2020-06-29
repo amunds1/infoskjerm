4f008a0e61fbd0c3948621140908d1414d1138c6
aa1202 <andreas.amundsen123@gmail.com>
2020-06-29 13:33:33 +0200

    Initial commit

---
+++ src/Bikes.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import createEnturService from "@entur/sdk";
+
+const service = createEnturService({ clientName: "amundsen-infoskjerm" });
+
+const Bikes = () => {
+  const [bikeStations, setBikeStations] = useState([]);
+
+  useEffect(() => {
+    service
+      .getBikeRentalStationsByPosition(
+        {
+          latitude: 63.428311,
+          longitude: 10.392514,
+        },
+        230
+      )
+      .then((data) => setBikeStations(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Bysykkel</h3>
+			
+      {bikeStations.map((stationData) => (
+        <Station key={stationData.id} station={stationData} />
+      ))}
+    </div>
+  );
+};
+
+export default Bikes;
+
+const Station = (props) => {
+  const { station } = props;
+
+  return <div>
+		{station.name} - {station.bikesAvailable} - {station.spacesAvailable}
+	</div>;
+};
